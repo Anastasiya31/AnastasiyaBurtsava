fe466a2b77b3af0d75641e5cc218f62eb48b2ba0
Anastasiya31 <burts3va.a@yandex.by>
2021-10-17 21:57:34 +0300

    исправленное ДЗ 5 урока

--- Lesson5/src/main/java/com/home/model/Computer.java
+++ Lesson5/src/main/java/com/home/model/Computer.java
@@ -1,32 +1,27 @@
 package com.home.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Random;
 import java.util.Scanner;
-import java.util.stream.IntStream;
 
+@Setter
+@Getter
 public class Computer {
-    String processor;
-    int ram;
-    String hdd;
-    static int period;
+    private String processor;
+    private int ram;
+    private String hdd;
+    private int period;
 
     public Computer(String processor, int ram, String hdd, int period) {
         this.processor = processor;
         this.ram = ram;
         this.hdd = hdd;
-        Computer.period = period;
+        this.period = period;
     }
 
-    @Override
-    public String toString() {
-        return "Компьютер: " +
-                "Процессор - '" + processor + '\'' +
-                ", оперативка = " + ram + "гб" +
-                ", жесткий диск - " + hdd + '\'' +
-                ", ресурс полных циклов работы = " + period;
-    }
-
-    public static void on() {
+    public void on() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Внимание! Введите 0 или 1");
         int n = sc.nextInt();
@@ -36,22 +31,34 @@ public class Computer {
         }
         Random random = new Random();
         int c = random.nextInt(2);
-        if (IntStream.range(0, period).anyMatch(i -> true)) {
+        if (period != 0) {
             if (n == c) {
                 System.out.println("Компьютер включился");
                 off();
             } else {
                 System.out.println("Компьютер сгорел!");
             }
+        } else {
+            System.out.println("Компьютер сгорел!");
         }
     }
 
-    public static void off() {
+    public void off() {
         System.out.println("Компьютер выключен");
         period--;
         if (period == 0) {
             System.out.println("Компьютер сгорел!");
-        } else
+        } else {
             on();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Компьютер: " +
+                "Процессор - '" + processor + '\'' +
+                ", оперативка = " + ram + "гб" +
+                ", жесткий диск - " + hdd + '\'' +
+                ", ресурс полных циклов работы = " + period;
     }
 }
