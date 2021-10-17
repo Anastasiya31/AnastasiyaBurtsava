c9c742c351975f41e24e018616c0c4d5ec33512c
Anastasiya31 <burts3va.a@yandex.by>
2021-10-17 22:06:16 +0300

    исправленное ДЗ 5 урока

--- Lesson5/src/main/java/com/home/model/Computer.java
+++ Lesson5/src/main/java/com/home/model/Computer.java
@@ -1,20 +1,56 @@
 package com.home.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Random;
 import java.util.Scanner;
-import java.util.stream.IntStream;
 
+@Setter
+@Getter
 public class Computer {
-    String processor;
-    int ram;
-    String hdd;
-    static int period;
+    private String processor;
+    private int ram;
+    private String hdd;
+    private int period;
 
     public Computer(String processor, int ram, String hdd, int period) {
         this.processor = processor;
         this.ram = ram;
         this.hdd = hdd;
-        Computer.period = period;
+        this.period = period;
+    }
+
+    public void on() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Внимание! Введите 0 или 1");
+        int n = sc.nextInt();
+        if (n > 1) {
+            System.out.println("Введите 0 или 1");
+            n = sc.nextInt();
+        }
+        Random random = new Random();
+        int c = random.nextInt(2);
+        if (period != 0) {
+            if (n == c) {
+                System.out.println("Компьютер включился");
+                off();
+            } else {
+                System.out.println("Компьютер сгорел!");
+            }
+        } else {
+            System.out.println("Компьютер сгорел!");
+        }
+    }
+
+    public void off() {
+        System.out.println("Компьютер выключен");
+        period--;
+        if (period == 0) {
+            System.out.println("Компьютер сгорел!");
+        } else {
+            on();
+        }
     }
 
     @Override
@@ -25,33 +61,5 @@ public class Computer {
                 ", жесткий диск - " + hdd + '\'' +
                 ", ресурс полных циклов работы = " + period;
     }
-
-    public static void on() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Внимание! Введите 0 или 1");
-        int n = sc.nextInt();
-        if (n > 1) {
-            System.out.println("Введите 0 или 1");
-            n = sc.nextInt();
-        }
-        Random random = new Random();
-        int c = random.nextInt(2);
-        if (IntStream.range(0, period).anyMatch(i -> true)) {
-            if (n == c) {
-                System.out.println("Компьютер включился");
-                off();
-            } else {
-                System.out.println("Компьютер сгорел!");
-            }
-        }
-    }
-
-    public static void off() {
-        System.out.println("Компьютер выключен");
-        period--;
-        if (period == 0) {
-            System.out.println("Компьютер сгорел!");
-        } else
-            on();
-    }
 }
+
