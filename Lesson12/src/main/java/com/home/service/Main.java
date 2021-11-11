79c339f8b14b0940a42829017306bb9b4eb6f423
Anastasiya31 <burts3va.a@yandex.by>
2021-11-11 22:41:27 +0300

    Дз 12 урока

---
+++ Lesson12/src/main/java/com/home/service/Main.java
@@ -0,0 +1,16 @@
+package com.home.service;
+
+import static com.home.service.TextFormatter.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "ghkkmsknckkxssh";
+        String text =
+                "Знакомьтесь с моом семьей. Нас пятеро – мои родители, мом старший брат, моя маленькая сестра и я. Сначала познакомьтесь с моими мамой и папой, Джейн и Майклом. Моя мама любит читать, а мой папа любит играть в шахматы с моим братом Кеном. Моя мама стройная. У нее длинные рыжие волосы. И большие карие глаза. У нее очень приятная улыбка и нежный голос. Моя мама очень добрая и понимающая ";
+
+        System.out.println(str.substring(str.indexOf('h'), str.lastIndexOf('s')));
+        System.out.println(str.replace(str.charAt(3), str.charAt(0)));
+        palindromeMass(new String[]{"Winter", "Spring", "summus", "Autumn", "esummuse", "summus"});
+        formatText(text);
+    }
+}
